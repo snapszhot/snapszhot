f80379fa7b97b1fc27485ff83fcd34bdb5757f85
skilar <paulhabeeb@hey.com>
2022-07-28 12:29:43 -0400

    Add animated GIF support (#2)
    The max file size allowed by Prismic is 10 MB. The changes in this PR allow for images to be uploaded to Imgur and used in lieu of Prismic.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import PropTypes from 'prop-types'
 
-import { PrismicProvider } from '@prismicio/react'
-import Client from '@lib/prismic'
-
 import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <PrismicProvider client={Client}>
-            <Component {...pageProps} />
-        </PrismicProvider>
-    )
+    return <Component {...pageProps} />
 }
 
 MyApp.propTypes = {
