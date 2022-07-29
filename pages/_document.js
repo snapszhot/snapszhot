aa678a66265419142e93b57c59c5ee63ee5c57cf
Paul Habeeb <paulhabeeb@hey.com>
2022-07-29 01:55:47 -0400

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang='en'>
-            <Head />
+            <Head>
+                <link rel='manifest' href='/manifest.webmanifest' />
+                <link rel='icon' href='/favicon.ico' sizes='any' />
+                <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+                <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
