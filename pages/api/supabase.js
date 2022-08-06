a85f326d83b3f83f7d432c41e3437c442416ec61
Paul Habeeb <paulhabeeb@hey.com>
2022-08-06 15:33:45 -0400

    Don’t update analytics during preview and dev

--- pages/api/supabase.js
+++ pages/api/supabase.js
@@ -2,18 +2,24 @@ import supabase from '@lib/supabase-client'
 
 export default async function handler(req, res) {
     try {
-        const { puzzle_id, won, frame_won } = req.body
+        const isPreview =
+            req?.cookies?.__next_preview_data != null &&
+            req?.cookies?.__prerender_bypass != null
 
-        const { error } = await supabase.from('analytics').insert([
-            {
-                puzzle_id,
-                won,
-                frame_won,
-            },
-        ])
+        if (process.env.ENVIRONMENT !== 'development' && !isPreview) {
+            const { puzzle_id, won, frame_won } = req.body
 
-        if (error) {
-            throw error
+            const { error } = await supabase.from('analytics').insert([
+                {
+                    puzzle_id,
+                    won,
+                    frame_won,
+                },
+            ])
+
+            if (error) {
+                throw error
+            }
         }
 
         res.status(200).json({ message: 'Success!' })
