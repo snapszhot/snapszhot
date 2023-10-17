edfeab683b996889410779df201f8b232e94ba85
snapszhot <snapswot@proton.me>
2023-10-17 23:30:58 +0200

    Add placeholder svg

--- components/views/Game/HintImages/HintImage.js
+++ components/views/Game/HintImages/HintImage.js
@@ -9,16 +9,38 @@ export default function HintImage({ dimensions, index, url }) {
     const MAX_HEIGHT = 320
     const MAX_WIDTH = (dimensions?.width / dimensions?.height) * MAX_HEIGHT
 
+    const placeholder = `data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27${MAX_WIDTH}%27%20height=%27${MAX_HEIGHT}%27/%3e`
+
     if (isDisplayed) {
         return (
             <div className={styles.container}>
-                <img
-                    className={styles.image}
-                    alt=''
-                    height={MAX_HEIGHT}
-                    src={url}
-                    width={MAX_WIDTH}
-                />
+                <span className={styles.outerSpan}>
+                    <span className={styles.innerSpan}>
+                        <img
+                            style={{
+                                display: 'block',
+                                maxWidth: '100%',
+                                width: 'initial',
+                                height: 'initial',
+                                background: 'none',
+                                opacity: 1,
+                                border: 0,
+                                margin: 0,
+                                padding: 0,
+                            }}
+                            alt=''
+                            aria-hidden={true}
+                            src={placeholder}
+                        />
+                    </span>
+                    <img
+                        className={styles.image}
+                        alt=''
+                        height={MAX_HEIGHT}
+                        src={url}
+                        width={MAX_WIDTH}
+                    />
+                </span>
             </div>
         )
     }
