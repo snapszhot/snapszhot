bd0c05febbadeece38cc55effa69f8b63a0256c5
snapszhot <snapswot@proton.me>
2025-12-23 09:59:03 -0600

    Remove unused export

--- components/views/index.js
+++ components/views/index.js
@@ -1,3 +1,3 @@
-export { default as Archive } from './Archive'
-export { Game, GameWithSWR } from './Game'
-export { default as FourOhFour } from './FourOhFour'
+export { default as Archive } from "./Archive";
+export { Game } from "./Game";
+export { default as FourOhFour } from "./FourOhFour";
