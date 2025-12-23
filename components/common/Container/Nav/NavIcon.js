da9a2c009e71dc8ea27b817be824baa1d79f9edc
snapszhot <snapswot@proton.me>
2025-12-23 09:07:50 -0600

    Bump dependencies, upgrade to Next.js v14

--- components/common/Container/Nav/NavIcon.js
+++ components/common/Container/Nav/NavIcon.js
@@ -1,21 +1,19 @@
-import PropTypes from 'prop-types'
-import Link from 'next/link'
+import PropTypes from "prop-types";
+import Link from "next/link";
 
 export default function NavIcon({ children, link, title }) {
-    return (
-        <li>
-            <Link href={link}>
-                <a>
-                    <span className='visuallyHidden'>{title}</span>
-                    {children}
-                </a>
-            </Link>
-        </li>
-    )
+  return (
+    <li>
+      <Link href={link}>
+        <span className="visuallyHidden">{title}</span>
+        {children}
+      </Link>
+    </li>
+  );
 }
 
 NavIcon.propTypes = {
-    children: PropTypes.node,
-    link: PropTypes.string,
-    title: PropTypes.string,
-}
+  children: PropTypes.node,
+  link: PropTypes.string,
+  title: PropTypes.string,
+};
