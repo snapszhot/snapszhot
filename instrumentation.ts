8c7b7bb9afb8380fad111205588a5a28c36d17af
snapszhot <snapswot@proton.me>
2026-05-16 12:43:52 +0200

    Update sentry config

--- instrumentation.ts
+++ instrumentation.ts
@@ -4,7 +4,10 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
   }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
 }
 
-// Capture errors from Server Components, middleware, and proxies
 export const onRequestError = Sentry.captureRequestError;
